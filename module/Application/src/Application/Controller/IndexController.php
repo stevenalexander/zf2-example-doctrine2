bbe05cf766221534b9a03161b20e62065432bcf5
Steven Alexander <steven.william.alexander@googlemail.com>
2013-08-28 12:02:41 +0100

    refactored index action

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -1,31 +1,32 @@
 <?php
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
 
 namespace Application\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Application\Entity\User;
 
 class IndexController extends AbstractActionController
 {
+    protected $_objectManager;
+
     public function indexAction()
     {
-        $objectManager = $this
-            ->getServiceLocator()
-            ->get('Doctrine\ORM\EntityManager');
-
-        $user = new \Application\Entity\User();
+        $user = new User();
         $user->setFullName('Simon Sample');
 
-        $objectManager->persist($user);
-        $objectManager->flush();
+        $this->getObjectManager()->persist($user);
+        $this->getObjectManager()->flush();
 
         die(var_dump($user->getId()));
+    }
+
+    protected function getObjectManager()
+    {
+        if (!$this->_objectManager) {
+            $this->_objectManager = $this->getServiceLocator()->get('Doctrine\ORM\EntityManager');
+        }
+
+        return $this->_objectManager;
     }
 }
